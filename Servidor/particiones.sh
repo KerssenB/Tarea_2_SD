9fe371fa2b918ab732cab50baba81b8063a28b70
KerssenB <kerssen.barros@mail.udp.cl>
2024-06-02 16:39:54 -0400

    fix: .sh

--- Servidor/particiones.sh
+++ Servidor/particiones.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
-# Esperar a que Kafka esté disponible
 while ! nc -z kafka 9092; do   
   sleep 1
 done
 
-# Crear el topic con múltiples particiones
 kafka-topics.sh --create --topic pedidos-topic --bootstrap-server kafka:9092 --partitions 3 --replication-factor 1 --if-not-exists
